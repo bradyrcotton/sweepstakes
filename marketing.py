d712d10f3e206c78f969d67ebcac914c0e33ab3a
Brady <bradyrcotton@gmail.com>
2021-04-02 02:11:56 -0500

    created methods for insert_sweepstakes and get_sweepstakes in sweepstakes_queue_manager

--- marketing.py
+++ marketing.py
@@ -1,1 +1,4 @@
 class MarketingFirm:
+
+    def __init__(self):
+        pass
--- sweepstakes_queue_manager.py
+++ sweepstakes_queue_manager.py
@@ -1,5 +1,15 @@
+from sweep_queue import Queue
+import user_interface
+
+
 class SweepstakesQueueManager:
 
-
     def __init__(self):
-        pass
+        self.queue = Queue()
+
+    def insert_sweepstakes(self):
+        sweepstake = user_interface.get_string_input('Please input sweepstake')
+        self.queue.enqueue(sweepstake)
+
+    def get_sweepstake(self):
+        self.queue.dequeue()
