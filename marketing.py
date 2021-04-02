c7824265c35b62bc35e6d8719718166951214073
Brady <bradyrcotton@gmail.com>
2021-04-02 02:37:53 -0500

    created create_sweepstakes method in MarketingFirm class

--- marketing.py
+++ marketing.py
@@ -1,4 +1,10 @@
+from marketing_firm_creator import MarketingFirmCreator
+
 class MarketingFirm:
 
     def __init__(self):
-        pass
+        self.manager = MarketingFirmCreator()
+
+    def create_sweepstakes(self):
+        sweep = self.manager.choose_manager()
+        return sweep 
